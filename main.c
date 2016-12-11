d7c19661d5976a872271efb0dd5ef84b96379b18
dburke2016 <dburke2016@fau.edu>
2016-12-11 18:54:08 -0500

    Add files via upload
    Initial Commit

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <msp430.h> 
+
+/*
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer
+
+
+
+    P1OUT = 0b00000000;  	// mov.b    #00000000b,&P1OUT
+    P1DIR = 0b11111111;  	// mov.b    #11111111b,&P1DIR
+    P2DIR = 0b00000000;  	// mov.b    #00000000b,&P2DIR
+
+    int BIT_MASK_DOORS = 0b00001111;
+    int BIT_MASK_LOCK = BIT4;
+    int Car_Doors = 0;
+    int Car_Lock = 0;
+    int is_pressed = 0;
+    while(1){
+    	Car_Doors=P2IN & BIT_MASK_DOORS;
+		Car_Lock=P2IN & BIT_MASK_LOCK;
+	
+
+		// if	car is locked doors all doors are closed and lock alarm is off
+		if(Car_Lock){
+			P1OUT = ~(BIT_MASK_DOORS | BIT_MASK_LOCK);
+		}else{
+		//else car P1OUT depends on the input from push buttons and lock alarm is on
+			P1OUT = ~Car_Doors | BIT_MASK_LOCK;
+		}
+    }
+}
+
+
